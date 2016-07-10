d98d0ccd9298d953e09bec0a041ea9edcef882d7
kcerfus <Megaman1!>
2016-07-10 18:20:40 -0500

    kyle commit

--- app/src/test/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/ExampleUnitTest.java
+++ app/src/test/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/ExampleUnitTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
- * TEST TESTfda
+ * TEST TESTfdafda
  */
 public class ExampleUnitTest {
     @Test

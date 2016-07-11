165d9fc858a7a1518d3bbcdafdfa562aed17ad98
noah-trilling <noah.trilling@gmail.com>
2016-07-11 14:02:44 -0500

    stubs for layouts. set main activity.

---
+++ app/src/main/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/DietInput.java
@@ -0,0 +1,13 @@
+package com.uwm.wundergrads.diabetesselfmanagement_wundergrads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DietInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dietinput);
+    }
+}

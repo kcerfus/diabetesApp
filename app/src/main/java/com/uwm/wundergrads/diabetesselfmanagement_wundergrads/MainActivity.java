4c8397e8f113950bab13439543eef8c5e7649e96
kcerfus <Megaman1!>
2016-08-02 18:23:23 -0500

    Updated UI, working on MainActivity

--- app/src/main/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/MainActivity.java
+++ app/src/main/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/MainActivity.java
@@ -2,6 +2,8 @@ package com.uwm.wundergrads.diabetesselfmanagement_wundergrads;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // This goes to DietFilter class no matter what button is clicked. Need to figure out how to go to
+        // specific class based on what button was pressed.
+        Intent intent = new Intent(MainActivity.this, DietFilter.class);
+        startActivity(intent);
     }
 }
--- app/src/test/java/main/MainActivity.java
+++ app/src/test/java/main/MainActivity.java
@@ -8,5 +8,13 @@ import android.support.v7.widget.Toolbar;
  * Created by kcerfus on 7/10/2016.
  */
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    /** Called when the user clicks the Send button */
+    public void sendMessage() {
+        // Do something in response to button
+    }
 }

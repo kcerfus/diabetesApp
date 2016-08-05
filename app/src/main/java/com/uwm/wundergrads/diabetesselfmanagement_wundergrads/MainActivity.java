dc841714e84d1a5c7309b2da48d54338758a9f20
noah-trilling <noah.trilling@gmail.com>
2016-08-04 21:26:20 -0500

    Loading BGL, Diet, Exercise, and Medication Fragments

--- app/src/main/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/MainActivity.java
+++ app/src/main/java/com/uwm/wundergrads/diabetesselfmanagement_wundergrads/MainActivity.java
@@ -2,6 +2,7 @@ package com.uwm.wundergrads.diabetesselfmanagement_wundergrads;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.InputQueue;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
@@ -17,27 +18,47 @@ public class MainActivity extends AppCompatActivity {
 
         BGL_button = (Button) findViewById(R.id.bgl_button);
         BGL_button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){ startActivity(new Intent(getApplicationContext(), BGLInput.class)); }
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), InputQuery.class);
+                intent.putExtra("mode", "BGL");
+                startActivity(intent);
+            }
         });
 
         Diet_button = (Button) findViewById(R.id.diet_button);
         Diet_button.setOnClickListener(new View.OnClickListener(){
-           public void onClick(View v){ startActivity(new Intent(getApplicationContext(), DietInput.class)); }
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), InputQuery.class);
+                intent.putExtra("mode", "Diet");
+                startActivity(intent);
+            }
         });
 
         Exercise_button = (Button) findViewById(R.id.exercise_button);
         Exercise_button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){ startActivity(new Intent(getApplicationContext(), ExerciseInput.class)); }
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), InputQuery.class);
+                intent.putExtra("mode", "Exercise");
+                startActivity(intent);
+            }
         });
 
         Medication_button = (Button) findViewById(R.id.medication_button);
         Medication_button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){ startActivity(new Intent(getApplicationContext(), MedicationInput.class)); }
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), InputQuery.class);
+                intent.putExtra("mode", "Medication");
+                startActivity(intent);
+            }
         });
 
         Regimen_button = (Button) findViewById(R.id.reg_button);
         Regimen_button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){ startActivity(new Intent(getApplicationContext(), ExerciseInput.class)); }
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), InputQuery.class);
+                intent.putExtra("mode", "BGL");
+                startActivity(intent);
+            }
         });
     }
 }
